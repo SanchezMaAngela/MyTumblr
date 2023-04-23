edacd89bd69ddb68dbada8f5e0500be900f2d57b
Angela <angelparsanchez@gmail.com>
2023-04-24 00:03:36 +0800

    php login script added

--- index.php
+++ index.php
@@ -1,3 +1,40 @@
+<?php
+
+	//START THE SESSION
+	SESSION_START();
+
+	//PRE DEFINED VARIABLE
+	$acc_username = "AngelaSanchez";
+	$acc_password = "gela143";
+	$acc_fullname = "Ma. Angela Sanchez";
+	$acc_address = "Marinduque";
+
+	$url_add = "http://".$_SERVER['HTTP_HOST'] .$_SERVER['PHP_SELF'];
+
+	if(isset($_REQUEST['login_button']) === true){
+		
+		if($_REQUEST['form_username'] != $acc_username ){
+			header("Location: " .$url_add. "?notexist");
+
+		}else if ($_REQUEST['form_username'] == $acc_username && $_REQUEST['form_password'] != $acc_password) {
+			header("Location: ".$url_add." ?wrongpass");
+
+		}else if ($_REQUEST['form_username'] == $acc_username && $_REQUEST['form_password'] == $acc_password) {
+			header("Location: ".$url_add." ?success");
+
+	//SESSION VARIABLES
+		$_SESSION['ses_username'] = $acc_username;
+		$_SESSION['ses_pass'] = $acc_password;
+		$_SESSION['ses_name'] = $acc_fullname;
+		$_SESSION['ses_address'] = $acc_address;
+
+		}
+
+	}
+
+//END OF LOG IN BUTTON
+?> 
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -24,12 +61,39 @@
 		      	</div>
 		      	<h3 class="text-center mb-4">MyTumblr Login</h3>
 						
-						<form action="#" class="login-form">
+
+						<form method= "POST" class="login-form">
+
 		      		<div class="form-group">
-		      			<input type="text" class="form-control rounded-left" placeholder="Username" required>
+
+		      			<?php
+
+		      			if(isset($_REQUEST['notexist']) === true){
+		      				echo "<div class= 'alert alert-danger' role= 'alert'> Username does not exist... </div>";
+
+		      			}else if (isset($_REQUEST['wrongpass'])=== true) {
+		      				echo "<div class= 'alert alert-warning' role= 'alert'> Incorrect Password... </div>";
+		      				
+		      			}else if (isset($_REQUEST['success'])=== true) {
+		      				echo "<div class= 'alert alert-success' role= 'alert'> Redirecting... </div>";
+		      				header("Refresh: 3; url=account.php");
+
+		      			}else if (isset($_REQUEST['logout'])=== true) {
+		      				echo "<div class= 'alert alert-info' role= 'alert'> Thank You! </div>";
+		      			}else if (isset($_REQUEST['logfirst'])=== true) {
+		      				echo "<div class= 'alert alert-info' role= 'alert'> Please log in first... </div>";
+		      			}else if (isset($_SESSION['ses_username'])=== true) {
+		      				echo "<div class= 'alert alert-warning' role= 'alert'> You are still logged in. <a href= 'account.php'> Click here to proceed.</a> </div>";
+		      			}
+
+
+		      			?>
+
+
+		      			<input type="text" class="form-control rounded-left" placeholder="Username" name= "form_username" required>
 		      		</div>
 	            <div class="form-group d-flex">
-	              <input type="password" class="form-control rounded-left" placeholder="Password" required>
+	              <input type="password" class="form-control rounded-left" placeholder="Password" name= "form_password" required>
 	            </div>
 	            <div class="form-group d-md-flex">
 	            	<div class="w-50">
@@ -43,9 +107,11 @@
 								</div>
 	            </div>
 	            <div class="form-group">
-	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5">Get Started</button>
+	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5" name= "login_button" >Get Started</button>
 	            </div>
 	          </form>
+
+
 	        </div>
 				</div>
 			</div>
